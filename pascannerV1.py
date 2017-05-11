86160574e20e2c996eceb776a0823492f3d1121c
Alejandro Franco Vázquez <alejandrofrancovazquez@gmail.com>
2017-05-11 20:31:07 +0200

    Added wp-theme detector

--- pascannerV1.py
+++ pascannerV1.py
@@ -5,6 +5,8 @@ import re
 import sys, getopt
 from argparse import ArgumentParser
 
+
+
 def attack():
     print "[*] Comenzando ataque.. "
 
@@ -18,11 +20,11 @@ def help():
         print (clave, valor)
 
 def search():
+
     url = 'https://www.paginasamarillas.es/search/informatica/all-ma/all-pr/all-is/all-ci/all-ba/all-pu/all-nc/'+pagina+'?cp='+lugar+'&what=informatica&where='+lugar+'&ub=false&qc=true'
     req = requests.get(url)
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
-
     if 'https://www.paginasamarillas.es/search/informatica/all-ma/all-pr/all-is/all-ci/all-ba/all-pu/all-nc/'+'1999'+'?cp='+lugar+'&what=informatica&where='+lugar+'&ub=false&qc=true' in url:
         links = soup.find_all('a', attrs={'itemprop': re.compile("^url")})
         for link in links:
@@ -66,18 +68,51 @@ def search():
                         soup2 = BeautifulSoup(html2, 'html.parser')
                         links = soup2.find_all('meta')
                         strLink = str(links)
+                        # Almacena el atributo content donde sale la version de WordPress
+                        WPVersions = soup2.find_all('meta', attrs={"content":re.compile("^WordPress.?.")})
+                        JOOVersions = soup2.find_all('meta', attrs={"content":re.compile("^Joomla.?.")})
+                        DRVersions = soup2.find_all('meta', attrs={"content":re.compile("^Drupal.?.")})
                         # Pide respuesta de linkk+/wp-admin/ y si da codigo 200 es que es una web wordpress
                         req2W = requests.get(linkk+"/wp-admin/")
-                        if "WordPress" in strLink or req2W.status_code == 200:
+                        # Mejora deteccion wordpress
+                        WPThemes = soup2.find_all('link', attrs={"href": re.compile(".?./wp-content/themes/.?.")})
+                        # Detector de temas
+                        print ""
+                        if "WordPress" in strLink or req2W == 200:
                             print link.get('href') + ' - [WORDPRESS]'
+                            for WPVersion in WPVersions:
+                                WPVersionContent = WPVersion["content"]
+                                if str(WPVersionContent) != "WordPress 4.7.4":
+                                    print chr(27)+"[0;36m"+"    [*] Version: "+chr(27)+"[0m"
+                                    print chr(27)+"[0;36m"+"       - "+WPVersion["content"]+chr(27)+"[0m"+chr(27)+"[6;31m"" [OUTDATED]"+chr(27)+"[0m"
+                                else:
+                                    print chr(27)+"[0;36m"+"    [*] Version: "+chr(27)+"[0m"
+                                    print chr(27)+"[0;36m"+"       - "+WPVersion["content"]+chr(27)+"[0m"
+                                if WPThemes:
+                                    print chr(27)+"[0;36m"+"    [*] Tema: "+chr(27)+"[0m"
+                                    for theme in WPThemes:
+                                        themeContent = theme["href"]
+                                        print "       - "+themeContent.split("/")[5]
+                                        break
+                                    break
+
                         elif "Joomla" in strLink:
                             print link.get('href') + ' - [JOOMLA]'
+                            for JOOVersion in JOOVersions:
+                                print chr(27)+"[0;36m"+"   - "+JOOVersion["content"]+chr(27)+"[0m"
                         elif "Drupal" in strLink:
                             print link.get('href') + ' - [DRUPAL]'
+                            for DRVersion in DRVersions:
+                                print chr(27)+"[0;36m"+"    [*] Version: "+chr(27)+"[0m"
+                                print chr(27)+"[0;36m"+"       - "+DRVersion["content"]+chr(27)+"[0m"
+                            if not DRVersions:
+                                print chr(27)+"[0;36m"+"    [*] Version: "+chr(27)+"[0m"
+                                print chr(27)+"[0;31m"+"       - No disponible"+chr(27)+"[0m"
                         elif "PrestaShop" in strLink:
                             print link.get('href') + ' - [PRESTASHOP]'
                         else:
-                            print link.get('href') + ' - [NOCMS]'
+                            print chr(27)+"[0;20m"+link.get('href') + ' - [NOCMS]'+chr(27)+"[0m"
+                            #print chr(27)+"[0;36m"+"   - "+"Possible SQLi on: "+link.get('href')+"?id=1"+chr(27)+"[0m"
 
             except requests.exceptions.ConnectionError as error:
                  continue
@@ -104,3 +139,4 @@ def main(argv):
 
 if __name__ == "__main__":
    main(sys.argv[1:])
+
